903c467e908b12186ac3b722a817465ee915bc67
Brayan Osmar Pereyra Suxo <brayan@colosa.com>
2013-01-03 11:07:23 -0400

    TEST FRAMEWORK I add the test of triggers
    TEST FRAMEWORK I add the test of triggers

--- tests/functional/src/main/java/com/colosa/qa/automatization/pages/DynaformExecution.java
+++ tests/functional/src/main/java/com/colosa/qa/automatization/pages/DynaformExecution.java
@@ -11,74 +11,64 @@ import com.colosa.qa.automatization.common.extJs.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class DynaformExecution extends Page{
+public class DynaformExecution extends Page {
 
-    public DynaformExecution() throws FileNotFoundException, IOException{
-        
+    public DynaformExecution() throws FileNotFoundException, IOException {
+
     }
 
-    public void intoDynaform() throws Exception{
+    // Pages.Main().goHome();
+
+    // into level of debug
+    public void intoDebug() throws Exception {
+        Browser.driver().switchTo().defaultContent();
+        Browser.driver().switchTo().frame("casesFrame");
+    }
+
+    // into level of pmTrack
+    public void intoPmtrack() throws Exception {
+        Browser.driver().switchTo().defaultContent();
+        Browser.driver().switchTo().frame("casesFrame");
+        Browser.driver().switchTo().frame("casesSubFrame");
+    }
+
+    // into level of dynaform    
+    public void intoDynaform() throws Exception {
         Browser.driver().switchTo().defaultContent();
         Browser.driver().switchTo().frame("casesFrame");
         Browser.driver().switchTo().frame("casesSubFrame");
         Browser.driver().switchTo().frame("openCaseFrame");
     }
 
-    public void intoDebug() throws Exception{
-        Browser.driver().switchTo().defaultContent();
-        Browser.driver().switchTo().frame("casesFrame");
-    }
-
-    public void outDynaform() throws Exception{
+    // out level of dynaform    
+    public void outDynaform() throws Exception {
         Browser.driver().switchTo().defaultContent();
     }
 
+    // get field of dynaform
     public WebElement getField(String nameField) throws Exception{
+        intoDynaform();
         String str = "";
         str = ConfigurationSettings.getInstance().getSetting("DynaformExecution.webElement.fieldDynaform");
         str = str.replace("replaceNameFieldDynaform", nameField);
         return Browser.getElement(str);
     }
 
+    // get property of field
     public String getFieldProperty(String nameField, String property) throws Exception{
+        intoDynaform();
         String str = "";
         str = ConfigurationSettings.getInstance().getSetting("DynaformExecution.webElement.fieldDynaform");
         str = str.replace("replaceNameFieldDynaform", nameField);
         return Browser.getElement(str).getAttribute(property);
     }
 
+    // get object of dynaform
     public WebElement getObject(String xpathField) throws Exception{
+        intoDynaform();
         String str = "";
         str = ConfigurationSettings.getInstance().getSetting("DynaformExecution.webElement.fieldDebug");
         str = str.replace("replaceXpath", xpathField);
         return Browser.getElement(str);
     }
-
-    public String searchValueVariableDebug(String nameVariable) throws Exception{
-        WebElement gridDebug = Browser.getElement("DynaformExecution.webElement.gridDebug");
-        List<WebElement> gridDebugDivs = gridDebug.findElements(By.tagName("div"));
-
-        String valueField = "";
-
-        for(WebElement divs:gridDebugDivs)
-        {
-            if( divs.getAttribute("innerHTML").trim().equals(nameVariable))
-            {
-                WebElement trParent = divs.findElement(By.xpath("..")).findElement(By.xpath(".."));
-                List<WebElement> fieldTds = trParent.findElements(By.tagName("td"));
-
-                for(WebElement tds:fieldTds)
-                {
-                    if(!tds.findElement(By.xpath(".//div")).getAttribute("innerHTML").trim().equals(nameVariable))
-                    {
-                        valueField = tds.findElement(By.xpath(".//div")).getAttribute("innerHTML").trim();
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-        return valueField;
-    }
-
 }
