5140a7db7415e54d5fea672a715e3cf2700ac0f6
Herbert Saal <herbert.saal@colosa.com>
2013-01-04 12:59:01 -0400

    Add process of the test of dependent fields

---
+++ tests/functional/src/main/resources/DependentFields/TestDependentFields.pm
@@ -0,0 +1,157 @@
+000004887O:8:"stdClass":26:{s:7:"process";a:28:{s:7:"PRO_UID";s:32:"36145458550d3897ab2fae4070717663";s:10:"PRO_PARENT";s:32:"36145458550d3897ab2fae4070717663";s:8:"PRO_TIME";d:1;s:12:"PRO_TIMEUNIT";s:4:"DAYS";s:10:"PRO_STATUS";s:6:"ACTIVE";s:12:"PRO_TYPE_DAY";s:0:"";s:8:"PRO_TYPE";s:6:"NORMAL";s:14:"PRO_ASSIGNMENT";s:5:"FALSE";s:12:"PRO_SHOW_MAP";i:0;s:16:"PRO_SHOW_MESSAGE";i:0;s:17:"PRO_SHOW_DELEGATE";i:0;s:17:"PRO_SHOW_DYNAFORM";i:0;s:12:"PRO_CATEGORY";s:0:"";s:16:"PRO_SUB_CATEGORY";s:0:"";s:12:"PRO_INDUSTRY";i:0;s:15:"PRO_UPDATE_DATE";N;s:15:"PRO_CREATE_DATE";s:19:"2012-12-20 17:56:10";s:15:"PRO_CREATE_USER";s:32:"00000000000000000000000000000001";s:10:"PRO_HEIGHT";i:5000;s:9:"PRO_WIDTH";i:10000;s:11:"PRO_TITLE_X";i:0;s:11:"PRO_TITLE_Y";i:0;s:9:"PRO_DEBUG";i:0;s:13:"PRO_DYNAFORMS";a:1:{s:7:"PROCESS";s:0:"";}s:25:"PRO_DERIVATION_SCREEN_TPL";s:0:"";s:15:"PRO_DESCRIPTION";s:19:"TestDependentFields";s:9:"PRO_TITLE";s:19:"TestDependentFields";s:18:"PRO_CATEGORY_LABEL";s:11:"No Category";}s:5:"tasks";a:1:{i:0;a:48:{s:7:"PRO_UID";s:32:"36145458550d3897ab2fae4070717663";s:7:"TAS_UID";s:32:"72365663450d38986c5b1c1062564734";s:8:"TAS_TYPE";s:6:"NORMAL";s:12:"TAS_DURATION";d:1;s:14:"TAS_DELAY_TYPE";s:0:"";s:14:"TAS_TEMPORIZER";d:0;s:12:"TAS_TYPE_DAY";s:0:"";s:12:"TAS_TIMEUNIT";s:4:"DAYS";s:9:"TAS_ALERT";s:5:"FALSE";s:21:"TAS_PRIORITY_VARIABLE";s:0:"";s:15:"TAS_ASSIGN_TYPE";s:8:"BALANCED";s:19:"TAS_ASSIGN_VARIABLE";s:30:"@@SYS_NEXT_USER_TO_BE_ASSIGNED";s:24:"TAS_MI_INSTANCE_VARIABLE";s:24:"@@SYS_VAR_TOTAL_INSTANCE";s:24:"TAS_MI_COMPLETE_VARIABLE";s:34:"@@SYS_VAR_TOTAL_INSTANCES_COMPLETE";s:19:"TAS_ASSIGN_LOCATION";s:5:"FALSE";s:25:"TAS_ASSIGN_LOCATION_ADHOC";s:5:"FALSE";s:16:"TAS_TRANSFER_FLY";s:5:"FALSE";s:17:"TAS_LAST_ASSIGNED";s:32:"00000000000000000000000000000001";s:8:"TAS_USER";s:1:"0";s:14:"TAS_CAN_UPLOAD";s:5:"FALSE";s:15:"TAS_VIEW_UPLOAD";s:5:"FALSE";s:33:"TAS_VIEW_ADDITIONAL_DOCUMENTATION";s:5:"FALSE";s:14:"TAS_CAN_CANCEL";s:5:"FALSE";s:13:"TAS_OWNER_APP";s:5:"FALSE";s:7:"STG_UID";s:0:"";s:13:"TAS_CAN_PAUSE";s:5:"FALSE";s:20:"TAS_CAN_SEND_MESSAGE";s:4:"TRUE";s:19:"TAS_CAN_DELETE_DOCS";s:5:"FALSE";s:16:"TAS_SELF_SERVICE";s:5:"FALSE";s:9:"TAS_START";s:4:"TRUE";s:16:"TAS_TO_LAST_USER";s:5:"FALSE";s:19:"TAS_SEND_LAST_EMAIL";s:5:"FALSE";s:14:"TAS_DERIVATION";s:6:"NORMAL";s:8:"TAS_POSX";i:388;s:8:"TAS_POSY";i:91;s:9:"TAS_WIDTH";i:165;s:10:"TAS_HEIGHT";i:40;s:9:"TAS_COLOR";s:0:"";s:11:"TAS_EVN_UID";s:0:"";s:12:"TAS_BOUNDARY";s:0:"";s:25:"TAS_DERIVATION_SCREEN_TPL";s:0:"";s:9:"TAS_TITLE";s:6:"Task 1";s:15:"TAS_DESCRIPTION";s:0:"";s:13:"TAS_DEF_TITLE";s:0:"";s:19:"TAS_DEF_DESCRIPTION";s:0:"";s:17:"TAS_DEF_PROC_CODE";s:0:"";s:15:"TAS_DEF_MESSAGE";s:0:"";s:23:"TAS_DEF_SUBJECT_MESSAGE";s:0:"";}}s:6:"routes";a:1:{i:0;a:17:{s:7:"ROU_UID";s:32:"13505967150d3898d497768023136921";s:10:"ROU_PARENT";s:1:"0";s:7:"PRO_UID";s:32:"36145458550d3897ab2fae4070717663";s:7:"TAS_UID";s:32:"72365663450d38986c5b1c1062564734";s:13:"ROU_NEXT_TASK";s:2:"-1";s:8:"ROU_CASE";s:1:"1";s:8:"ROU_TYPE";s:10:"SEQUENTIAL";s:13:"ROU_CONDITION";s:0:"";s:16:"ROU_TO_LAST_USER";s:5:"FALSE";s:12:"ROU_OPTIONAL";s:5:"FALSE";s:14:"ROU_SEND_EMAIL";s:4:"TRUE";s:16:"ROU_SOURCEANCHOR";s:1:"1";s:16:"ROU_TARGETANCHOR";s:1:"0";s:11:"ROU_TO_PORT";s:1:"1";s:13:"ROU_FROM_PORT";s:1:"2";s:11:"ROU_EVN_UID";s:0:"";s:7:"GAT_UID";s:0:"";}}s:5:"lanes";a:0:{}s:8:"gateways";a:0:{}s:6:"inputs";a:0:{}s:7:"outputs";a:0:{}s:9:"dynaforms";a:1:{i:0;a:6:{s:7:"DYN_UID";s:32:"46427988150d3899e434900062396162";s:7:"PRO_UID";s:32:"36145458550d3897ab2fae4070717663";s:8:"DYN_TYPE";s:7:"xmlform";s:12:"DYN_FILENAME";s:65:"36145458550d3897ab2fae4070717663/46427988150d3899e434900062396162";s:9:"DYN_TITLE";s:19:"TestDependentFields";s:15:"DYN_DESCRIPTION";s:0:"";}}s:5:"steps";a:1:{i:0;a:8:{s:8:"STEP_UID";s:32:"73721776950d3a8333e8a97008445055";s:7:"PRO_UID";s:32:"36145458550d3897ab2fae4070717663";s:7:"TAS_UID";s:32:"72365663450d38986c5b1c1062564734";s:13:"STEP_TYPE_OBJ";s:8:"DYNAFORM";s:12:"STEP_UID_OBJ";s:32:"46427988150d3899e434900062396162";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:1;s:9:"STEP_MODE";s:4:"EDIT";}}s:8:"triggers";a:0:{}s:9:"taskusers";a:1:{i:0;a:4:{s:7:"TAS_UID";s:32:"72365663450d38986c5b1c1062564734";s:7:"USR_UID";s:32:"16601507650c85281879f33027092433";s:7:"TU_TYPE";s:1:"1";s:11:"TU_RELATION";s:1:"2";}}s:8:"groupwfs";a:1:{i:0;a:5:{s:7:"GRP_UID";s:32:"16601507650c85281879f33027092433";s:10:"GRP_STATUS";s:6:"ACTIVE";s:11:"GRP_LDAP_DN";s:0:"";s:6:"GRP_UX";s:6:"NORMAL";s:9:"GRP_TITLE";s:14:"Administrators";}}s:12:"steptriggers";a:0:{}s:13:"dbconnections";a:0:{}s:12:"reportTables";a:0:{}s:16:"reportTablesVars";a:0:{}s:14:"stepSupervisor";a:0:{}s:17:"objectPermissions";a:0:{}s:10:"subProcess";a:0:{}s:11:"caseTracker";a:0:{}s:17:"caseTrackerObject";a:0:{}s:5:"stage";a:0:{}s:14:"fieldCondition";a:0:{}s:5:"event";a:0:{}s:13:"caseScheduler";a:0:{}s:15:"processCategory";N;}00000003246427988150d3899e434900062396162000004019<?xml version="1.0" encoding="UTF-8"?>
+<dynaForm type="xmlform" name="36145458550d3897ab2fae4070717663/46427988150d3899e434900062396162" width="500" enabletemplate="0" mode="" nextstepsave="prompt">
+  <dropdowns type="subtitle" enablehtml="0" btn_cancel="Cancel">
+    <en>Dropdowns</en>
+  </dropdowns>
+  <country type="dropdown" required="0" readonly="0" mode="edit" btn_cancel="Cancel" dependentfields="state" defaultvalue="" hint="" sqlconnection="" savelabel="0">SELECT IC_UID, IC_NAME FROM ISO_COUNTRY<en>Country</en>
+</country>
+  <state type="dropdown" required="0" readonly="0" mode="edit" btn_cancel="Cancel" dependentfields="location" defaultvalue="" hint="" sqlconnection="" savelabel="0">SELECT IS_UID, IS_NAME FROM ISO_SUBDIVISION WHERE IC_UID = @@country ORDER BY IS_NAME<en>State</en>
+</state>
+  <location type="dropdown" required="0" readonly="0" mode="edit" btn_cancel="Cancel" dependentfields="" defaultvalue="" hint="" sqlconnection="" savelabel="0">  SELECT IL_UID, IL_NAME FROM ISO_LOCATION WHERE IC_UID = @@country AND IS_UID = @@state<en>Location</en>
+</location>
+  <suggestfields type="subtitle" enablehtml="0" btn_cancel="Cancel">
+    <en>Suggest Field</en>
+  </suggestfields>
+<suggest_country type="suggest" required="0" savelabel="0" size="15" mode="edit" maxresults="6" shownoresults="0" store_new_entry="0" primary_key_type="0" btn_cancel="Cancel" dependentfields="suggest_state,suggest_state2" hint="" sqlconnection="" table="" primary_key="" callback="" table_data="" primary_key_data="" primary_key_type_data="" field="" readonly="0"> SELECT IC_UID, IC_NAME FROM ISO_COUNTRY 
+  <en>Country Suggest</en>
+</suggest_country>
+<suggest_state type="dropdown" required="0" readonly="0" mode="edit" btn_cancel="Cancel" dependentfields="suggest_location" defaultvalue="" hint="" sqlconnection="" savelabel="0">SELECT IS_UID, IS_NAME FROM ISO_SUBDIVISION WHERE IC_UID = @@suggest_country ORDER BY IS_NAME<en>State</en>
+</suggest_state>
+<suggest_location type="dropdown" required="0" readonly="0" mode="edit" btn_cancel="Cancel" dependentfields="" defaultvalue="" hint="" sqlconnection="" savelabel="0">  SELECT IL_UID, IL_NAME FROM ISO_LOCATION WHERE IC_UID = @@suggest_country AND IS_UID = @@suggest_state<en>Location</en>
+</suggest_location>
+<suggest_state2 type="dropdown" required="0" readonly="0" mode="edit" btn_cancel="Cancel" dependentfields="suggest_location2" defaultvalue="" hint="" sqlconnection="" savelabel="0">SELECT IS_UID, IS_NAME FROM ISO_SUBDIVISION WHERE IC_UID = @@suggest_country ORDER BY IS_NAME<en>State 2</en>
+</suggest_state2>
+<suggest_location2 type="dropdown" required="0" readonly="0" mode="edit" btn_cancel="Cancel" dependentfields="" defaultvalue="" hint="" sqlconnection="" savelabel="0">  SELECT IL_UID, IL_NAME FROM ISO_LOCATION WHERE IC_UID = @@suggest_country AND IS_UID = @@suggest_state2<en>Location 2</en>
+</suggest_location2>
+  <textfields type="subtitle" enablehtml="0" btn_cancel="Cancel">
+    <en>Text Fields</en>
+  </textfields>  
+<username type="text" maxlength="64" validate="Any" required="0" readonly="0" size="15" mode="edit" btn_cancel="Cancel" dependentfields="userCompleteName,userDepartment">  
+  <en>User Name (PM login)</en>
+</username>
+  <userCompleteName type="text" maxlength="64" validate="Any" required="0" readonly="0" size="15" mode="edit" btn_cancel="Cancel">SELECT CONCAT(USR_FIRSTNAME, ' ', USR_LASTNAME) FROM USERS WHERE USR_USERNAME = @@username  
+  <en>User Complete Name</en>
+</userCompleteName>
+<userDepartment type="text" maxlength="200" validate="Any" required="0" readonly="0" size="80" mode="edit" btn_cancel="Cancel" mask="" strto="" dependentfields="" defaultvalue="" hint="" formula="" function="" sqlconnection="" savelabel="0"><![CDATA[SELECT CONTENT.CON_VALUE FROM CONTENT LEFT JOIN USERS ON CONTENT.CON_ID = USERS.DEP_UID AND CONTENT.CON_CATEGORY = 'DEPO_TITLE' WHERE USERS.USR_USERNAME = @@username
+  ]]><en>User Department</en>
+</userDepartment>
+<ok type="submit" btn_cancel="Cancel">
+  <en>Ok</en>
+</ok>
+</dynaForm>00000003246427988150d3899e434900062396162000005111   <form id="{$form_id}" name="{$form_name}" action="{$form_action}" class="formDefault" method="post" enctype="multipart/form-data" style="margin:0px;" onsubmit="return validateForm('{$form_objectRequiredFields}');">  <div class="borderForm" style="width:{$form_width}; padding-left:0; padding-right:0; border-width:{$form_border};">
+    <div class="boxTop"><div class="a"></div><div class="b"></div><div class="c"></div></div>
+    <div class="content" style="height:{$form_height};">
+    <table width="99%">
+      <tbody><tr>
+        <td valign="top">
+          <input class="notValidateThisFields" name="__notValidateThisFields__" id="__notValidateThisFields__" value="{$form_objectRequiredFields}" type="hidden">
+          <input name="DynaformRequiredFields" id="DynaformRequiredFields" value="{$form_objectRequiredFields}" type="hidden">
+          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                                    <tbody><tr>
+              <td colspan="2" class="withoutLabel">{$form.__DYNAFORM_OPTIONS}</td>
+            </tr>
+                                                <tr>
+              <td class="FormSubTitle" colspan="2" id="form[dropdowns]" align="">
+                <span>{$form.dropdowns}</span>
+                              </td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$country}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.country}</td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$state}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.state}</td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$location}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.location}</td>
+            </tr>
+                                                <tr>
+              <td class="FormSubTitle" colspan="2" id="form[suggestfields]" align="">
+                <span>{$form.suggestfields}</span>
+                              </td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$suggest_country}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.suggest_country}</td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$suggest_state}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.suggest_state}</td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$suggest_location}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.suggest_location}</td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$suggest_state2}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.suggest_state2}</td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$suggest_location2}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.suggest_location2}</td>
+            </tr>
+                                                <tr>
+              <td class="FormSubTitle" colspan="2" id="form[textfields]" align="">
+                <span>{$form.textfields}</span>
+                              </td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$username}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.username}</td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$userCompleteName}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.userCompleteName}</td>
+            </tr>
+                                                <tr>
+              <td class="FormLabel" width="{$form_labelWidth}">{$userDepartment}</td>
+              <td class="FormFieldContent" width="{$form_fieldContentWidth}">{$form.userDepartment}</td>
+            </tr>
+                                  </tbody></table>
+        </td>
+      </tr>
+    </tbody></table>
+           </div>
+       <div class="boxBottom"><div class="a"></div><div class="b"></div><div class="c"></div></div>
+       </div>
+                                                                                                                                                                                                                                                         </form>MAILTEMPL000000018alert_message.html000000682
+<table style=\"background-color: white; font-family: Arial,Helvetica,sans-serif; color: black; font-size: 11px; text-align: left;\" cellpadding='10' cellspacing='0' width='100%'>
+<tbody>	
+		<tr><td style='font-size: 14px;'><b>ALERT MESSAGE</b></td></tr>
+		<tr><td style='vertical-align:middel;'>
+				<hr>
+				<br />
+						The Case <b>@#APP_NUMBER</b>, currently in the task <b>@#TAS_TITLE</b>, expired on <b>@#DEL_TASK_DUE_DATE</b>.						
+				<br />		
+				<br />
+				<hr><b>This Business Process is powered by ProcessMaker.<b><br />
+						<a href='http://www.processmaker.com' style='color:#c40000;'>www.processmaker.com</a>
+				<br />
+		</td></tr>
+</tbody>
+</table>000000022unassignedMessage.html000000829<table width="100%" cellpadding="10" cellspacing="0" style="background: #FFFFFF; font: 11px arial, verdana, helvetica, sans-serif;">
+    <tr>
+        <td style="font-size: 14px;">
+            <strong>UNASSIGNED CASE</strong>
+            <hr />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            The case <strong>#@#APP_NUMBER</strong> is available on the unassigned list.
+            <br />
+            <br />
+            Note: If you want to claim the case and it is not available on the list, it might be already taken.
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <hr />
+            This Business Process is powered by ProcessMaker.
+            <br />
+            <a href="http://www.processmaker.com" style="color: #C40000;">www.processmaker.com</a>
+        </td>
+    </tr>
+</table>
