dcbbfd740c075037880e4d73bd6e31ff1fa8a905
Brayan Osmar Pereyra Suxo <brayan@colosa.com>
2013-01-09 10:53:24 -0400

    Automation: External Step
    I add process to automation

--- tests/functional/src/main/java/com/colosa/qa/automatization/pages/Admin.java
+++ tests/functional/src/main/java/com/colosa/qa/automatization/pages/Admin.java
@@ -10,33 +10,82 @@ import com.colosa.qa.automatization.common.extJs.ExtJSGrid;
 
 public class Admin extends Main{
 
-	public Admin() throws Exception{
-	}
+    public Admin() throws Exception{
+    }
 
-	public static void goToLogs() throws Exception{
-		Browser.driver().switchTo().defaultContent();
-		Browser.driver().switchTo().frame("adminFrame");
-		WebElement we = Browser.driver().findElement(By.xpath("//*[@id='west-panel']/div[1]/div[2]/ul/li[@id='west-panel__logs']/a[2]"));
-		we.click();
-		//Browser.driver().switchTo().defaultContent();
+    public static void goToLogs() throws Exception{
+        Browser.driver().switchTo().defaultContent();
+        Browser.driver().switchTo().frame("adminFrame");
+        WebElement we = Browser.driver().findElement(By.xpath("//*[@id='west-panel']/div[1]/div[2]/ul/li[@id='west-panel__logs']/a[2]"));
+        we.click();
+        //Browser.driver().switchTo().defaultContent();
+    }
 
-	}
+    public static void goToPlugins() throws Exception{
+        Browser.driver().switchTo().defaultContent();
+        Browser.driver().switchTo().frame("adminFrame");
+        WebElement we = Browser.driver().findElement(By.xpath("//*[@id='west-panel__plugins']/a[2]"));
+        we.click();
+    }
 
+    public static void activePlugin(String namePlugin, Boolean flagActive) throws Exception{
+        goToPlugins();
+        Thread.sleep(3000);
+        Browser.driver().switchTo().defaultContent();
+        Browser.driver().switchTo().frame("adminFrame");
+        Browser.driver().switchTo().frame("setup-frame");
+        WebElement divFils = Browser.driver().findElement(By.xpath("//*[@id='processesGrid']/div/div[2]/div/div/div[2]"));
 
-	public static String eventStatus(Integer numCase) throws Exception{
+        List<WebElement> divsGrid = divFils.findElements(By.tagName("div"));
+        Boolean flagExist = false;
+        for(WebElement divs:divsGrid)
+        {
+            if ( (divs.getAttribute("class").indexOf ("x-grid3-cell-inner") > -1) && 
+                 (divs.getAttribute("innerHTML").indexOf (namePlugin) > -1) ) {
+                WebElement tablePlugin = divs.findElement(By.xpath("..")).findElement(By.xpath("..")).findElement(By.xpath(".."));
+                tablePlugin.click();
+                flagExist = true;
+                break;
+            }
+        }
 
-		Browser.driver().switchTo().defaultContent();
-		Browser.driver().switchTo().frame("adminFrame");
-		Browser.driver().switchTo().frame("setup-frame");
-		ExtJSGrid grid = new ExtJSGrid(Browser.driver().findElement(By.id("eventsGrid")), Browser.driver());
-		String status;
-		WebElement row = grid.getRowByColumnValue("Case Title", "#" + Integer.toString(numCase));
-		if(row==null)
-			throw new Exception("Case # "+Integer.toString(numCase)+" not found in Event Logs");
-		status = row.findElement(By.xpath("table/tbody/tr/td[13]/div")).getText().trim();
-		Browser.driver().switchTo().defaultContent();
-		return status;
-	}
+        if (flagExist) {
+            WebElement buttonActive = Browser.driver().findElement(By.id("activator"));
+            WebElement buttonLabel = buttonActive.findElement(By.tagName("button"));
+            
+            if ( buttonLabel.getAttribute("innerHTML").trim().indexOf ("Enable") > -1 && (flagActive) ) {
+                buttonLabel.click();
+                Thread.sleep(3000);
+                System.out.println("Plugin " + namePlugin + " active :)");
+            } else if ( buttonLabel.getAttribute("innerHTML").trim().indexOf ("Disable") > -1 && (flagActive) ) {
+                System.out.println("Plugin " + namePlugin + " is active :|");
+            } else if ( buttonLabel.getAttribute("innerHTML").trim().indexOf ("Enable") > -1 && (!(flagActive)) ) {
+                System.out.println("Plugin " + namePlugin + " is desactive :|");
+            } else if ( buttonLabel.getAttribute("innerHTML").trim().indexOf ("Disable") > -1 && (!(flagActive)) ) {
+                buttonLabel.click();
+                Thread.sleep(3000);
+                System.out.println("Plugin " + namePlugin + " desactive :)");
+            }
+            
+        } else {
+            System.out.println("Plugin " + namePlugin + " not exist :(");
+        }
+    }
+
+    public static String eventStatus(Integer numCase) throws Exception{
+
+        Browser.driver().switchTo().defaultContent();
+        Browser.driver().switchTo().frame("adminFrame");
+        Browser.driver().switchTo().frame("setup-frame");
+        ExtJSGrid grid = new ExtJSGrid(Browser.driver().findElement(By.id("eventsGrid")), Browser.driver());
+        String status;
+        WebElement row = grid.getRowByColumnValue("Case Title", "#" + Integer.toString(numCase));
+        if(row==null)
+            throw new Exception("Case # "+Integer.toString(numCase)+" not found in Event Logs");
+        status = row.findElement(By.xpath("table/tbody/tr/td[13]/div")).getText().trim();
+        Browser.driver().switchTo().defaultContent();
+        return status;
+    }
 
 
 
