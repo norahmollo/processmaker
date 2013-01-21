6f54d32e317c81fec3c6e4dd94c7345e38182cd4
Norah Mollo <norah@colosa.com>
2013-01-21 15:31:08 -0400

    Test PMFSend Message assert added and processes for resources

---
+++ tests/functional/src/main/resources/outputDocuments/Test_OutputDocument.pm
@@ -0,0 +1,226 @@
+000015912O:8:"stdClass":26:{s:7:"process";a:33:{s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:10:"PRO_PARENT";s:32:"58512775550e531cdc68fd8024776324";s:8:"PRO_TIME";d:1;s:12:"PRO_TIMEUNIT";s:4:"DAYS";s:10:"PRO_STATUS";s:6:"ACTIVE";s:12:"PRO_TYPE_DAY";s:0:"";s:8:"PRO_TYPE";s:6:"NORMAL";s:14:"PRO_ASSIGNMENT";s:5:"FALSE";s:12:"PRO_SHOW_MAP";i:0;s:16:"PRO_SHOW_MESSAGE";i:0;s:14:"PRO_SUBPROCESS";i:0;s:15:"PRO_TRI_DELETED";s:0:"";s:16:"PRO_TRI_CANCELED";s:0:"";s:14:"PRO_TRI_PAUSED";s:0:"";s:18:"PRO_TRI_REASSIGNED";s:0:"";s:17:"PRO_SHOW_DELEGATE";i:0;s:17:"PRO_SHOW_DYNAFORM";i:0;s:12:"PRO_CATEGORY";s:0:"";s:16:"PRO_SUB_CATEGORY";s:0:"";s:12:"PRO_INDUSTRY";i:0;s:15:"PRO_UPDATE_DATE";N;s:15:"PRO_CREATE_DATE";s:19:"2013-01-03 03:22:53";s:15:"PRO_CREATE_USER";s:32:"00000000000000000000000000000001";s:10:"PRO_HEIGHT";i:5000;s:9:"PRO_WIDTH";i:10000;s:11:"PRO_TITLE_X";i:0;s:11:"PRO_TITLE_Y";i:0;s:9:"PRO_DEBUG";i:0;s:13:"PRO_DYNAFORMS";a:1:{s:7:"PROCESS";s:0:"";}s:25:"PRO_DERIVATION_SCREEN_TPL";s:0:"";s:15:"PRO_DESCRIPTION";s:0:"";s:9:"PRO_TITLE";s:19:"Test OutputDocument";s:18:"PRO_CATEGORY_LABEL";s:11:"No Category";}s:5:"tasks";a:2:{i:0;a:53:{s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"81926742850f479cf795108046233497";s:8:"TAS_TYPE";s:6:"NORMAL";s:12:"TAS_DURATION";d:1;s:14:"TAS_DELAY_TYPE";s:0:"";s:14:"TAS_TEMPORIZER";d:0;s:12:"TAS_TYPE_DAY";s:0:"";s:12:"TAS_TIMEUNIT";s:4:"DAYS";s:9:"TAS_ALERT";s:5:"FALSE";s:21:"TAS_PRIORITY_VARIABLE";s:0:"";s:15:"TAS_ASSIGN_TYPE";s:8:"BALANCED";s:19:"TAS_ASSIGN_VARIABLE";s:30:"@@SYS_NEXT_USER_TO_BE_ASSIGNED";s:18:"TAS_GROUP_VARIABLE";s:26:"@@SYS_GROUP_TO_BE_ASSIGNED";s:24:"TAS_MI_INSTANCE_VARIABLE";s:24:"@@SYS_VAR_TOTAL_INSTANCE";s:24:"TAS_MI_COMPLETE_VARIABLE";s:34:"@@SYS_VAR_TOTAL_INSTANCES_COMPLETE";s:19:"TAS_ASSIGN_LOCATION";s:5:"FALSE";s:25:"TAS_ASSIGN_LOCATION_ADHOC";s:5:"FALSE";s:16:"TAS_TRANSFER_FLY";s:5:"FALSE";s:17:"TAS_LAST_ASSIGNED";s:32:"00000000000000000000000000000001";s:8:"TAS_USER";s:1:"0";s:14:"TAS_CAN_UPLOAD";s:5:"FALSE";s:15:"TAS_VIEW_UPLOAD";s:5:"FALSE";s:33:"TAS_VIEW_ADDITIONAL_DOCUMENTATION";s:5:"FALSE";s:14:"TAS_CAN_CANCEL";s:5:"FALSE";s:13:"TAS_OWNER_APP";s:5:"FALSE";s:7:"STG_UID";s:0:"";s:13:"TAS_CAN_PAUSE";s:5:"FALSE";s:20:"TAS_CAN_SEND_MESSAGE";s:4:"TRUE";s:19:"TAS_CAN_DELETE_DOCS";s:5:"FALSE";s:16:"TAS_SELF_SERVICE";s:5:"FALSE";s:9:"TAS_START";s:4:"TRUE";s:16:"TAS_TO_LAST_USER";s:5:"FALSE";s:19:"TAS_SEND_LAST_EMAIL";s:5:"FALSE";s:14:"TAS_DERIVATION";s:6:"NORMAL";s:8:"TAS_POSX";i:538;s:8:"TAS_POSY";i:88;s:9:"TAS_WIDTH";i:165;s:10:"TAS_HEIGHT";i:40;s:9:"TAS_COLOR";s:0:"";s:11:"TAS_EVN_UID";s:0:"";s:12:"TAS_BOUNDARY";s:0:"";s:25:"TAS_DERIVATION_SCREEN_TPL";s:0:"";s:23:"TAS_SELFSERVICE_TIMEOUT";i:0;s:20:"TAS_SELFSERVICE_TIME";s:0:"";s:25:"TAS_SELFSERVICE_TIME_UNIT";s:0:"";s:27:"TAS_SELFSERVICE_TRIGGER_UID";s:0:"";s:9:"TAS_TITLE";s:6:"Task 1";s:15:"TAS_DESCRIPTION";s:0:"";s:13:"TAS_DEF_TITLE";s:0:"";s:19:"TAS_DEF_DESCRIPTION";s:0:"";s:17:"TAS_DEF_PROC_CODE";s:0:"";s:15:"TAS_DEF_MESSAGE";s:0:"";s:23:"TAS_DEF_SUBJECT_MESSAGE";s:0:"";}i:1;a:53:{s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:8:"TAS_TYPE";s:6:"NORMAL";s:12:"TAS_DURATION";d:1;s:14:"TAS_DELAY_TYPE";s:0:"";s:14:"TAS_TEMPORIZER";d:0;s:12:"TAS_TYPE_DAY";s:0:"";s:12:"TAS_TIMEUNIT";s:4:"DAYS";s:9:"TAS_ALERT";s:5:"FALSE";s:21:"TAS_PRIORITY_VARIABLE";s:0:"";s:15:"TAS_ASSIGN_TYPE";s:8:"BALANCED";s:19:"TAS_ASSIGN_VARIABLE";s:30:"@@SYS_NEXT_USER_TO_BE_ASSIGNED";s:18:"TAS_GROUP_VARIABLE";s:26:"@@SYS_GROUP_TO_BE_ASSIGNED";s:24:"TAS_MI_INSTANCE_VARIABLE";s:24:"@@SYS_VAR_TOTAL_INSTANCE";s:24:"TAS_MI_COMPLETE_VARIABLE";s:34:"@@SYS_VAR_TOTAL_INSTANCES_COMPLETE";s:19:"TAS_ASSIGN_LOCATION";s:5:"FALSE";s:25:"TAS_ASSIGN_LOCATION_ADHOC";s:5:"FALSE";s:16:"TAS_TRANSFER_FLY";s:5:"FALSE";s:17:"TAS_LAST_ASSIGNED";s:32:"00000000000000000000000000000001";s:8:"TAS_USER";s:1:"0";s:14:"TAS_CAN_UPLOAD";s:5:"FALSE";s:15:"TAS_VIEW_UPLOAD";s:5:"FALSE";s:33:"TAS_VIEW_ADDITIONAL_DOCUMENTATION";s:5:"FALSE";s:14:"TAS_CAN_CANCEL";s:5:"FALSE";s:13:"TAS_OWNER_APP";s:5:"FALSE";s:7:"STG_UID";s:0:"";s:13:"TAS_CAN_PAUSE";s:5:"FALSE";s:20:"TAS_CAN_SEND_MESSAGE";s:4:"TRUE";s:19:"TAS_CAN_DELETE_DOCS";s:5:"FALSE";s:16:"TAS_SELF_SERVICE";s:5:"FALSE";s:9:"TAS_START";s:5:"FALSE";s:16:"TAS_TO_LAST_USER";s:5:"FALSE";s:19:"TAS_SEND_LAST_EMAIL";s:5:"FALSE";s:14:"TAS_DERIVATION";s:6:"NORMAL";s:8:"TAS_POSX";i:537;s:8:"TAS_POSY";i:168;s:9:"TAS_WIDTH";i:165;s:10:"TAS_HEIGHT";i:40;s:9:"TAS_COLOR";s:0:"";s:11:"TAS_EVN_UID";s:0:"";s:12:"TAS_BOUNDARY";s:0:"";s:25:"TAS_DERIVATION_SCREEN_TPL";s:0:"";s:23:"TAS_SELFSERVICE_TIMEOUT";i:0;s:20:"TAS_SELFSERVICE_TIME";s:0:"";s:25:"TAS_SELFSERVICE_TIME_UNIT";s:0:"";s:27:"TAS_SELFSERVICE_TRIGGER_UID";s:0:"";s:9:"TAS_TITLE";s:6:"Task 2";s:15:"TAS_DESCRIPTION";s:0:"";s:13:"TAS_DEF_TITLE";s:0:"";s:19:"TAS_DEF_DESCRIPTION";s:0:"";s:17:"TAS_DEF_PROC_CODE";s:0:"";s:15:"TAS_DEF_MESSAGE";s:0:"";s:23:"TAS_DEF_SUBJECT_MESSAGE";s:0:"";}}s:6:"routes";a:2:{i:0;a:17:{s:7:"ROU_UID";s:32:"43032937450f48c39c1d750008298543";s:10:"ROU_PARENT";s:1:"0";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"81926742850f479cf795108046233497";s:13:"ROU_NEXT_TASK";s:32:"61550862950f48c2e9b0507098798073";s:8:"ROU_CASE";s:1:"1";s:8:"ROU_TYPE";s:10:"SEQUENTIAL";s:13:"ROU_CONDITION";s:0:"";s:16:"ROU_TO_LAST_USER";s:5:"FALSE";s:12:"ROU_OPTIONAL";s:5:"FALSE";s:14:"ROU_SEND_EMAIL";s:4:"TRUE";s:16:"ROU_SOURCEANCHOR";s:1:"1";s:16:"ROU_TARGETANCHOR";s:1:"0";s:11:"ROU_TO_PORT";s:1:"1";s:13:"ROU_FROM_PORT";s:1:"2";s:11:"ROU_EVN_UID";s:0:"";s:7:"GAT_UID";s:0:"";}i:1;a:17:{s:7:"ROU_UID";s:32:"61036136550f48c35c4dab9019764073";s:10:"ROU_PARENT";s:1:"0";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:13:"ROU_NEXT_TASK";s:2:"-1";s:8:"ROU_CASE";s:1:"1";s:8:"ROU_TYPE";s:10:"SEQUENTIAL";s:13:"ROU_CONDITION";s:0:"";s:16:"ROU_TO_LAST_USER";s:5:"FALSE";s:12:"ROU_OPTIONAL";s:5:"FALSE";s:14:"ROU_SEND_EMAIL";s:4:"TRUE";s:16:"ROU_SOURCEANCHOR";s:1:"1";s:16:"ROU_TARGETANCHOR";s:1:"0";s:11:"ROU_TO_PORT";s:1:"1";s:13:"ROU_FROM_PORT";s:1:"2";s:11:"ROU_EVN_UID";s:0:"";s:7:"GAT_UID";s:0:"";}}s:5:"lanes";a:0:{}s:8:"gateways";a:0:{}s:6:"inputs";a:0:{}s:7:"outputs";a:2:{i:0;a:24:{s:11:"OUT_DOC_UID";s:32:"50032098350f89a977f8ce0013336140";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:24:"OUT_DOC_REPORT_GENERATOR";s:8:"HTML2PDF";s:17:"OUT_DOC_LANDSCAPE";i:0;s:13:"OUT_DOC_MEDIA";s:6:"Letter";s:19:"OUT_DOC_LEFT_MARGIN";i:15;s:20:"OUT_DOC_RIGHT_MARGIN";i:15;s:18:"OUT_DOC_TOP_MARGIN";i:15;s:21:"OUT_DOC_BOTTOM_MARGIN";i:15;s:16:"OUT_DOC_GENERATE";s:4:"BOTH";s:12:"OUT_DOC_TYPE";s:4:"HTML";s:24:"OUT_DOC_CURRENT_REVISION";i:0;s:21:"OUT_DOC_FIELD_MAPPING";N;s:18:"OUT_DOC_VERSIONING";i:0;s:24:"OUT_DOC_DESTINATION_PATH";s:0:"";s:12:"OUT_DOC_TAGS";s:0:"";s:28:"OUT_DOC_PDF_SECURITY_ENABLED";i:0;s:34:"OUT_DOC_PDF_SECURITY_OPEN_PASSWORD";s:0:"";s:35:"OUT_DOC_PDF_SECURITY_OWNER_PASSWORD";s:0:"";s:32:"OUT_DOC_PDF_SECURITY_PERMISSIONS";s:0:"";s:13:"OUT_DOC_TITLE";s:27:"Test_OUTPUTDOCUMENT_version";s:19:"OUT_DOC_DESCRIPTION";s:0:"";s:16:"OUT_DOC_FILENAME";s:12:"TEST_version";s:16:"OUT_DOC_TEMPLATE";s:220:"<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+</head>
+<body>
+<p>TEST: @@APPLICATION</p>
+<p>USER: @@USER_LOGGED</p>
+</body>
+</html>";}i:1;a:24:{s:11:"OUT_DOC_UID";s:32:"28028020550f4850800ad81003195208";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:24:"OUT_DOC_REPORT_GENERATOR";s:8:"HTML2PDF";s:17:"OUT_DOC_LANDSCAPE";i:0;s:13:"OUT_DOC_MEDIA";s:6:"Letter";s:19:"OUT_DOC_LEFT_MARGIN";i:15;s:20:"OUT_DOC_RIGHT_MARGIN";i:15;s:18:"OUT_DOC_TOP_MARGIN";i:15;s:21:"OUT_DOC_BOTTOM_MARGIN";i:15;s:16:"OUT_DOC_GENERATE";s:4:"BOTH";s:12:"OUT_DOC_TYPE";s:4:"HTML";s:24:"OUT_DOC_CURRENT_REVISION";i:0;s:21:"OUT_DOC_FIELD_MAPPING";N;s:18:"OUT_DOC_VERSIONING";i:1;s:24:"OUT_DOC_DESTINATION_PATH";s:0:"";s:12:"OUT_DOC_TAGS";s:0:"";s:28:"OUT_DOC_PDF_SECURITY_ENABLED";i:0;s:34:"OUT_DOC_PDF_SECURITY_OPEN_PASSWORD";s:0:"";s:35:"OUT_DOC_PDF_SECURITY_OWNER_PASSWORD";s:0:"";s:32:"OUT_DOC_PDF_SECURITY_PERMISSIONS";s:0:"";s:13:"OUT_DOC_TITLE";s:23:"TESTING OUTPUT DOCUMENT";s:19:"OUT_DOC_DESCRIPTION";s:0:"";s:16:"OUT_DOC_FILENAME";s:4:"TEST";s:16:"OUT_DOC_TEMPLATE";s:495:"
+<center><b>TEST OUTPUT DOCUMENT</b><center>
+<p>This is the test for Templates per Output Documents.</p>
+
+<table border="1" width="80%">
+	<tbody><tr>
+		<td width="40%">
+		  <b>Column One</b>
+		</td>
+		
+		<td width="60%">
+			<b>Column Two</b>
+		</td>
+	</tr>
+	
+	<tr>
+		<td width="40%">
+		  Column One
+		</td>
+		
+		<td width="60%">
+			Column Two
+		</td>
+	</tr>
+	
+	<tr>
+		<td width="40%">
+		  Column One
+		</td>
+		
+		<td width="60%">
+			Column Two
+		</td>
+	</tr>
+</tbody></table>
+
+</center></center>";}}s:9:"dynaforms";a:1:{i:0;a:6:{s:7:"DYN_UID";s:32:"60552309650f87a60a72797027901032";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:8:"DYN_TYPE";s:7:"xmlform";s:12:"DYN_FILENAME";s:65:"93240728750f479cf78f2a4052410159/60552309650f87a60a72797027901032";s:9:"DYN_TITLE";s:23:"OUTPUT DOCUMENT CONTENT";s:15:"DYN_DESCRIPTION";s:0:"";}}s:5:"steps";a:5:{i:0;a:8:{s:8:"STEP_UID";s:32:"13880206350f87b735e3d94037758025";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:13:"STEP_TYPE_OBJ";s:8:"DYNAFORM";s:12:"STEP_UID_OBJ";s:32:"60552309650f87a60a72797027901032";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:3;s:9:"STEP_MODE";s:4:"EDIT";}i:1;a:8:{s:8:"STEP_UID";s:32:"87179078350f48737721b81015073503";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"81926742850f479cf795108046233497";s:13:"STEP_TYPE_OBJ";s:15:"OUTPUT_DOCUMENT";s:12:"STEP_UID_OBJ";s:32:"28028020550f4850800ad81003195208";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:1;s:9:"STEP_MODE";s:0:"";}i:2;a:8:{s:8:"STEP_UID";s:32:"93830395350f8b545849106091978085";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"81926742850f479cf795108046233497";s:13:"STEP_TYPE_OBJ";s:15:"OUTPUT_DOCUMENT";s:12:"STEP_UID_OBJ";s:32:"50032098350f89a977f8ce0013336140";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:2;s:9:"STEP_MODE";s:0:"";}i:3;a:8:{s:8:"STEP_UID";s:32:"36203749950f8b550efee15099446334";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:13:"STEP_TYPE_OBJ";s:15:"OUTPUT_DOCUMENT";s:12:"STEP_UID_OBJ";s:32:"50032098350f89a977f8ce0013336140";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:2;s:9:"STEP_MODE";s:0:"";}i:4;a:8:{s:8:"STEP_UID";s:32:"84449653750f8b66fdfcca0055250176";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:13:"STEP_TYPE_OBJ";s:15:"OUTPUT_DOCUMENT";s:12:"STEP_UID_OBJ";s:32:"28028020550f4850800ad81003195208";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:1;s:9:"STEP_MODE";s:0:"";}}s:8:"triggers";a:3:{i:0;a:7:{s:7:"TRI_UID";s:32:"62576572750f48fee5661f0075454063";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:8:"TRI_TYPE";s:6:"SCRIPT";s:10:"TRI_WEBBOT";s:764:"$caseId = @@APPLICATION;
+$outDocId = '28028020550f4850800ad81003195208';
+
+$query = "SELECT 
+APP_DOC_UID, 
+DOC_VERSION AS DOC_VERSION 
+FROM 
+APP_DOCUMENT 
+WHERE 
+APP_UID = '".$caseId."' 
+AND DOC_UID = '".$outDocId."' 
+AND DOC_VERSION = (SELECT MAX(DOC_VERSION) 
+                   FROM APP_DOCUMENT 
+                   WHERE APP_UID = '".$caseId."' AND DOC_UID = '".$outDocId."')";
+
+$result = executeQuery($query);
+
+if (count($result) > 0) { 
+   $path = PATH_DOCUMENT.$caseId."/outdocs/".$result[1]['APP_DOC_UID']."_".$result[1]['DOC_VERSION'].".html";
+   $contents = file_get_contents($path);
+   if (!$contents) {
+      echo("Unable to open file");
+   } else {
+      @@OUTPUT_CONTENT = $contents;
+   }
+}
+else
+   echo("Unable to find Output Document in database.");";s:9:"TRI_PARAM";s:0:"";s:9:"TRI_TITLE";s:14:"CONTENT OUTPUT";s:15:"TRI_DESCRIPTION";s:0:"";}i:1;a:7:{s:7:"TRI_UID";s:32:"55571851450f8a979ac86e2037512360";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:8:"TRI_TYPE";s:6:"SCRIPT";s:10:"TRI_WEBBOT";s:230:"$query="SELECT 
+CON_VALUE 
+FROM 
+CONTENT 
+WHERE 
+CON_ID = '28028020550f4850800ad81003195208' AND
+CON_CATEGORY='OUT_DOC_TEMPLATE' AND 
+CON_LANG='EN'";
+
+$result = executeQuery($query);
+
+@@OUTPUT_TEMPLATE = $result[1]['CON_VALUE'];
+
+";s:9:"TRI_PARAM";s:0:"";s:9:"TRI_TITLE";s:12:"GET TEMPLATE";s:15:"TRI_DESCRIPTION";s:0:"";}i:2;a:7:{s:7:"TRI_UID";s:32:"74337406750f8b6cf093e77034032550";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:8:"TRI_TYPE";s:6:"SCRIPT";s:10:"TRI_WEBBOT";s:725:"$caseId = @@APPLICATION;
+$outDocId = '28028020550f4850800ad81003195208';
+$outDocIdVersion = '50032098350f89a977f8ce0013336140';
+
+$query = "SELECT 
+DOC_VERSION AS DOC_VERSION 
+FROM 
+APP_DOCUMENT 
+WHERE 
+APP_UID = '".$caseId."' 
+AND DOC_UID = '".$outDocId."' ";
+
+$result = executeQuery($query);
+
+$queryVersion = "SELECT 
+DOC_VERSION AS DOC_VERSION 
+FROM 
+APP_DOCUMENT 
+WHERE 
+APP_UID = '".$caseId."' 
+AND DOC_UID = '".$outDocIdVersion."' ";
+
+$resultVersion = executeQuery($queryVersion);
+
+if (count($result) > 1) { 
+   @@VERSION="EXIST VERSION";
+}
+else {
+   echo("Error!!! Does not exist Version.");
+}
+
+if (count($resultVersion) == 1) { 
+   @@NOT_VERSION="DOES NOT EXIST VERSION";
+}
+else {
+   echo("Error!!! Exist Version.");
+}";s:9:"TRI_PARAM";s:0:"";s:9:"TRI_TITLE";s:11:"GET VERSION";s:15:"TRI_DESCRIPTION";s:0:"";}}s:9:"taskusers";a:2:{i:0;a:4:{s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:7:"USR_UID";s:32:"16601507650c85281879f33027092433";s:7:"TU_TYPE";s:1:"1";s:11:"TU_RELATION";s:1:"2";}i:1;a:4:{s:7:"TAS_UID";s:32:"81926742850f479cf795108046233497";s:7:"USR_UID";s:32:"16601507650c85281879f33027092433";s:7:"TU_TYPE";s:1:"1";s:11:"TU_RELATION";s:1:"2";}}s:8:"groupwfs";a:1:{i:0;a:5:{s:7:"GRP_UID";s:32:"16601507650c85281879f33027092433";s:10:"GRP_STATUS";s:6:"ACTIVE";s:11:"GRP_LDAP_DN";s:0:"";s:6:"GRP_UX";s:6:"NORMAL";s:9:"GRP_TITLE";s:14:"Administrators";}}s:12:"steptriggers";a:3:{i:0;a:6:{s:8:"STEP_UID";s:32:"13880206350f87b735e3d94037758025";s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:7:"TRI_UID";s:32:"55571851450f8a979ac86e2037512360";s:7:"ST_TYPE";s:6:"BEFORE";s:12:"ST_CONDITION";s:0:"";s:11:"ST_POSITION";s:1:"3";}i:1;a:6:{s:8:"STEP_UID";s:32:"13880206350f87b735e3d94037758025";s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:7:"TRI_UID";s:32:"62576572750f48fee5661f0075454063";s:7:"ST_TYPE";s:6:"BEFORE";s:12:"ST_CONDITION";s:0:"";s:11:"ST_POSITION";s:1:"2";}i:2;a:6:{s:8:"STEP_UID";s:32:"13880206350f87b735e3d94037758025";s:7:"TAS_UID";s:32:"61550862950f48c2e9b0507098798073";s:7:"TRI_UID";s:32:"74337406750f8b6cf093e77034032550";s:7:"ST_TYPE";s:6:"BEFORE";s:12:"ST_CONDITION";s:0:"";s:11:"ST_POSITION";s:1:"1";}}s:13:"dbconnections";a:0:{}s:12:"reportTables";a:0:{}s:16:"reportTablesVars";a:0:{}s:14:"stepSupervisor";a:0:{}s:17:"objectPermissions";a:0:{}s:10:"subProcess";a:0:{}s:11:"caseTracker";a:0:{}s:17:"caseTrackerObject";a:0:{}s:5:"stage";a:0:{}s:14:"fieldCondition";a:0:{}s:5:"event";a:1:{i:0;a:21:{s:7:"EVN_UID";s:32:"38752494050f479cf971460015114076";s:7:"PRO_UID";s:32:"93240728750f479cf78f2a4052410159";s:10:"EVN_STATUS";s:6:"ACTIVE";s:15:"EVN_WHEN_OCCURS";s:6:"SINGLE";s:14:"EVN_RELATED_TO";s:8:"MULTIPLE";s:7:"TAS_UID";s:0:"";s:16:"EVN_TAS_UID_FROM";s:32:"81926742850f479cf795108046233497";s:14:"EVN_TAS_UID_TO";s:0:"";s:26:"EVN_TAS_ESTIMATED_DURATION";d:0;s:13:"EVN_TIME_UNIT";s:4:"DAYS";s:8:"EVN_WHEN";d:1;s:16:"EVN_MAX_ATTEMPTS";i:3;s:10:"EVN_ACTION";s:0:"";s:14:"EVN_CONDITIONS";N;s:21:"EVN_ACTION_PARAMETERS";b:0;s:7:"TRI_UID";s:0:"";s:8:"EVN_POSX";i:620;s:8:"EVN_POSY";i:142;s:8:"EVN_TYPE";s:17:"bpmnEventEmptyEnd";s:11:"TAS_EVN_UID";s:0:"";s:15:"EVN_DESCRIPTION";s:0:"";}}s:13:"caseScheduler";a:0:{}s:15:"processCategory";N;}00000003260552309650f87a60a72797027901032000000869<?xml version="1.0" encoding="UTF-8"?>
+<dynaForm type="xmlform" name="93240728750f479cf78f2a4052410159/60552309650f87a60a72797027901032" width="500" enabletemplate="0" mode="" nextstepsave="prompt">
+  <TEST_OUTPUT type="title" enablehtml="0">
+    <en>Test Output Document</en>
+  </TEST_OUTPUT>
+  <OUTPUT_CONTENT type="textarea" required="0" readonly="0" rows="10" cols="50" mode="edit">
+    <en>Content </en>
+  </OUTPUT_CONTENT>
+  <OUTPUT_TEMPLATE type="textarea" required="0" readonly="0" rows="10" cols="50" mode="edit">
+    <en>Template </en>
+  </OUTPUT_TEMPLATE>
+  <NOT_VERSION type="text" maxlength="64" validate="Any" size="30" mode="edit">
+    <en>Not Version</en>
+  </NOT_VERSION>
+  <VERSION type="text" maxlength="64" validate="Any" size="30" mode="edit">
+    <en>Version</en>
+  </VERSION>
+  <SUBMIT type="submit">
+    <en>SUBMIT</en>
+  </SUBMIT>  
+</dynaForm>MAILTEMPL000000017TestTemplate.html000000475<html>
+<center><b>TEST OUTPUT DOCUMENT</b><center>
+<p>This is the test for Templates per Output Documents.</p>
+
+<table width="80%" border="1">
+	<tr>
+		<td width="40%">
+		  <b>Column One</b>
+		</td>
+		
+		<td width="60%">
+			<b>Column Two</b>
+		</td>
+	</tr>
+	
+	<tr>
+		<td width="40%">
+		  Column One
+		</td>
+		
+		<td width="60%">
+			Column Two
+		</td>
+	</tr>
+	
+	<tr>
+		<td width="40%">
+		  Column One
+		</td>
+		
+		<td width="60%">
+			Column Two
+		</td>
+	</tr>
+</table>
+
+</html>000000018alert_message.html000000009
+<table 000000015outputTest.html000000000000000012testing.html000000593<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+</head>
+<body>
+<center><b>TEST OUTPUT DOCUMENT</b><center>
+<p>This is the test for Templates per Output Documents.</p>
+<table style="width: 80%;" border="1">
+<tbody>
+<tr>
+<td width="40%"><b>Column One</b></td>
+<td width="60%"><b>Column Two</b></td>
+</tr>
+<tr>
+<td width="40%">Column One</td>
+<td width="60%">Column Two</td>
+</tr>
+<tr>
+<td width="40%">Column One</td>
+<td width="60%">Column Two</td>
+</tr>
+</tbody>
+</table>
+</center></center>
+</body>
+</html>000000022unassignedMessage.html000000829<table width="100%" cellpadding="10" cellspacing="0" style="background: #FFFFFF; font: 11px arial, verdana, helvetica, sans-serif;">
+    <tr>
+        <td style="font-size: 14px;">
+            <strong>UNASSIGNED CASE</strong>
+            <hr />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            The case <strong>#@#APP_NUMBER</strong> is available on the unassigned list.
+            <br />
+            <br />
+            Note: If you want to claim the case and it is not available on the list, it might be already taken.
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <hr />
+            This Business Process is powered by ProcessMaker.
+            <br />
+            <a href="http://www.processmaker.com" style="color: #C40000;">www.processmaker.com</a>
+        </td>
+    </tr>
+</table>
