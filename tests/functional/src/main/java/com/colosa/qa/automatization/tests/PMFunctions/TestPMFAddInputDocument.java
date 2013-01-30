97dfb0971959c5f13e8264fd39f9db54877a30e7
Ernesto Vega <ernesto.vegap@outlook.com>
2013-01-30 16:09:23 -0400

    Create test for function PMFAddInputDocument
    Added file TestPMFAddInputDocument.java
    Create function fileExist() into InputDocumentList.java

---
+++ tests/functional/src/main/java/com/colosa/qa/automatization/tests/PMFunctions/TestPMFAddInputDocument.java
@@ -0,0 +1,33 @@
+package com.colosa.qa.automatization.tests.PMFunctions;
+
+import org.junit.Assert;
+import org.junit.AfterClass;
+import org.junit.Test;
+import java.util.*;
+import com.colosa.qa.automatization.pages.*;
+import com.colosa.qa.automatization.common.*;
+import org.openqa.selenium.WebElement;
+import java.text.DecimalFormat;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TestPMFAddInputDocument{
+
+	@Test
+	public void runProcess()throws FileNotFoundException, IOException, Exception{
+
+		Pages.Login().gotoUrl();
+		Pages.Login().loginUser("admin", "admin", "workflow");
+		Pages.Main().goHome();	
+		Pages.Home().startCase("Bug 8283 - PMFAddInputDocument function request (Task 1)");
+		Pages.DynaformExecution().intoDynaform();
+		Pages.DynaformExecution().setFieldValue("Nombre", "Felipe");
+		Pages.DynaformExecution().setFieldValue("Apellido", "Hernandez");
+		Pages.DynaformExecution().setFieldValue("Descripcion", "Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba Prueba");
+		Pages.DynaformExecution().setFieldValue("send", "");
+		Assert.assertTrue(Pages.InputDocumentList().fileExists("bug8283.txt"));
+
+	}
+
+}
