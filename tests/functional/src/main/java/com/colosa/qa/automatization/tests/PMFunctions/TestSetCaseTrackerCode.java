b9f3948532542f4f44c31a842b75d1f2fa15f8d7
Ademar Hurtado <ademar.hurtado@gmail.com>
2013-02-05 11:58:01 -0400

    Add test and process for setCaseTrackerCode

---
+++ tests/functional/src/main/java/com/colosa/qa/automatization/tests/PMFunctions/TestSetCaseTrackerCode.java
@@ -0,0 +1,51 @@
+package com.colosa.qa.automatization.tests.PMFunctions;
+
+import org.junit.Assert;
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import com.colosa.qa.automatization.pages.*;
+import com.colosa.qa.automatization.common.*;
+import org.openqa.selenium.WebElement;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class TestSetCaseTrackerCode{
+
+	protected static int caseNum;
+	protected static String pin;
+	protected static String code;	
+
+	@Test
+	public void executeSetCaseTrackerCode() throws FileNotFoundException, IOException, Exception{
+		//Init case
+		Pages.Login().gotoUrl();
+		Pages.Login().loginUser("admin", "admin", "workflow");
+		Pages.Main().goHome();	
+		caseNum = Pages.Home().startCase("setCaseTrackerCode (Task 1)");
+		Pages.DynaformExecution().intoDynaform();
+	    Pages.InputDocProcess().continuebtn();
+		Pages.Main().goHome();
+		Pages.InputDocProcess().switchToDefault();
+		Assert.assertTrue("The case does not exist in Inbox", Pages.Home().existCase(caseNum));
+		Pages.Home().openCase(caseNum);
+		Pages.DynaformExecution().intoDynaform();
+		pin = Value.getValue(FieldKeyType.ID, "form[pin]");	
+		code = Value.getValue(FieldKeyType.ID, "form[code]");	
+		Pages.DynaformExecution().setFieldValue("continue", "");
+		Pages.DynaformExecution().intoDynaform();
+	    Pages.InputDocProcess().continuebtn();	
+		Pages.InputDocProcess().switchToDefault();
+		Pages.CaseTracker().goTo();
+		Pages.DynaformExecution().setFieldValue("CASE", code);
+		Pages.DynaformExecution().setFieldValue("PIN", pin);
+		Pages.DynaformExecution().setFieldValue("BSUBMIT", "");
+		System.out.println(Pages.Designer().getTaskColorStatus("Task 1"));
+		Assert.assertEquals("Completed Task", Pages.Designer().getTaskColorStatus("Task 1"));
+		System.out.println(Pages.Designer().getTaskColorStatus("Task 2"));
+		Assert.assertEquals("Pending Task / Not Executed", Pages.Designer().getTaskColorStatus("Task 2"));
+
+	}
+
+}
