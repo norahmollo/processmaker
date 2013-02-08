ed667d81d13af88fb380c0b824c535cd111c018b
Norah Mollo <norah@colosa.com>
2013-02-08 13:36:56 -0400

    Test PMF Add Case Note and Processes for resources

---
+++ tests/functional/src/main/resources/PMFunctions/Test_PMFRedirectToStep.pm
@@ -0,0 +1,120 @@
+000009553O:8:"stdClass":26:{s:7:"process";a:33:{s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:10:"PRO_PARENT";s:32:"90503094551098570ac1a80013327982";s:8:"PRO_TIME";d:1;s:12:"PRO_TIMEUNIT";s:4:"DAYS";s:10:"PRO_STATUS";s:6:"ACTIVE";s:12:"PRO_TYPE_DAY";s:0:"";s:8:"PRO_TYPE";s:6:"NORMAL";s:14:"PRO_ASSIGNMENT";s:5:"FALSE";s:12:"PRO_SHOW_MAP";i:0;s:16:"PRO_SHOW_MESSAGE";i:0;s:14:"PRO_SUBPROCESS";i:0;s:15:"PRO_TRI_DELETED";s:0:"";s:16:"PRO_TRI_CANCELED";s:0:"";s:14:"PRO_TRI_PAUSED";s:0:"";s:18:"PRO_TRI_REASSIGNED";s:0:"";s:17:"PRO_SHOW_DELEGATE";i:0;s:17:"PRO_SHOW_DYNAFORM";i:0;s:12:"PRO_CATEGORY";s:0:"";s:16:"PRO_SUB_CATEGORY";s:0:"";s:12:"PRO_INDUSTRY";i:0;s:15:"PRO_UPDATE_DATE";N;s:15:"PRO_CREATE_DATE";s:19:"2013-01-30 16:41:20";s:15:"PRO_CREATE_USER";s:32:"00000000000000000000000000000001";s:10:"PRO_HEIGHT";i:5000;s:9:"PRO_WIDTH";i:10000;s:11:"PRO_TITLE_X";i:0;s:11:"PRO_TITLE_Y";i:0;s:9:"PRO_DEBUG";i:0;s:13:"PRO_DYNAFORMS";a:1:{s:7:"PROCESS";s:0:"";}s:25:"PRO_DERIVATION_SCREEN_TPL";s:0:"";s:15:"PRO_DESCRIPTION";s:0:"";s:9:"PRO_TITLE";s:22:"Test PMFRedirectToStep";s:18:"PRO_CATEGORY_LABEL";s:11:"No Category";}s:5:"tasks";a:1:{i:0;a:53:{s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:7:"TAS_UID";s:32:"3599909135109857407f524015663549";s:8:"TAS_TYPE";s:6:"NORMAL";s:12:"TAS_DURATION";d:1;s:14:"TAS_DELAY_TYPE";s:0:"";s:14:"TAS_TEMPORIZER";d:0;s:12:"TAS_TYPE_DAY";s:0:"";s:12:"TAS_TIMEUNIT";s:4:"DAYS";s:9:"TAS_ALERT";s:5:"FALSE";s:21:"TAS_PRIORITY_VARIABLE";s:0:"";s:15:"TAS_ASSIGN_TYPE";s:8:"BALANCED";s:19:"TAS_ASSIGN_VARIABLE";s:30:"@@SYS_NEXT_USER_TO_BE_ASSIGNED";s:18:"TAS_GROUP_VARIABLE";s:0:"";s:24:"TAS_MI_INSTANCE_VARIABLE";s:24:"@@SYS_VAR_TOTAL_INSTANCE";s:24:"TAS_MI_COMPLETE_VARIABLE";s:34:"@@SYS_VAR_TOTAL_INSTANCES_COMPLETE";s:19:"TAS_ASSIGN_LOCATION";s:5:"FALSE";s:25:"TAS_ASSIGN_LOCATION_ADHOC";s:5:"FALSE";s:16:"TAS_TRANSFER_FLY";s:5:"FALSE";s:17:"TAS_LAST_ASSIGNED";s:32:"00000000000000000000000000000001";s:8:"TAS_USER";s:1:"0";s:14:"TAS_CAN_UPLOAD";s:5:"FALSE";s:15:"TAS_VIEW_UPLOAD";s:5:"FALSE";s:33:"TAS_VIEW_ADDITIONAL_DOCUMENTATION";s:5:"FALSE";s:14:"TAS_CAN_CANCEL";s:5:"FALSE";s:13:"TAS_OWNER_APP";s:5:"FALSE";s:7:"STG_UID";s:0:"";s:13:"TAS_CAN_PAUSE";s:5:"FALSE";s:20:"TAS_CAN_SEND_MESSAGE";s:4:"TRUE";s:19:"TAS_CAN_DELETE_DOCS";s:5:"FALSE";s:16:"TAS_SELF_SERVICE";s:5:"FALSE";s:9:"TAS_START";s:4:"TRUE";s:16:"TAS_TO_LAST_USER";s:5:"FALSE";s:19:"TAS_SEND_LAST_EMAIL";s:5:"FALSE";s:14:"TAS_DERIVATION";s:6:"NORMAL";s:8:"TAS_POSX";i:477;s:8:"TAS_POSY";i:87;s:9:"TAS_WIDTH";i:165;s:10:"TAS_HEIGHT";i:40;s:9:"TAS_COLOR";s:0:"";s:11:"TAS_EVN_UID";s:0:"";s:12:"TAS_BOUNDARY";s:0:"";s:25:"TAS_DERIVATION_SCREEN_TPL";s:0:"";s:23:"TAS_SELFSERVICE_TIMEOUT";i:0;s:20:"TAS_SELFSERVICE_TIME";s:0:"";s:25:"TAS_SELFSERVICE_TIME_UNIT";s:0:"";s:27:"TAS_SELFSERVICE_TRIGGER_UID";s:0:"";s:9:"TAS_TITLE";s:6:"Task 1";s:15:"TAS_DESCRIPTION";s:0:"";s:13:"TAS_DEF_TITLE";s:0:"";s:19:"TAS_DEF_DESCRIPTION";s:0:"";s:17:"TAS_DEF_PROC_CODE";s:0:"";s:15:"TAS_DEF_MESSAGE";s:0:"";s:23:"TAS_DEF_SUBJECT_MESSAGE";s:0:"";}}s:6:"routes";a:1:{i:0;a:17:{s:7:"ROU_UID";s:32:"5523131755109858526c818012810961";s:10:"ROU_PARENT";s:1:"0";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:7:"TAS_UID";s:32:"3599909135109857407f524015663549";s:13:"ROU_NEXT_TASK";s:2:"-1";s:8:"ROU_CASE";s:1:"1";s:8:"ROU_TYPE";s:10:"SEQUENTIAL";s:13:"ROU_CONDITION";s:0:"";s:16:"ROU_TO_LAST_USER";s:5:"FALSE";s:12:"ROU_OPTIONAL";s:5:"FALSE";s:14:"ROU_SEND_EMAIL";s:4:"TRUE";s:16:"ROU_SOURCEANCHOR";s:1:"1";s:16:"ROU_TARGETANCHOR";s:1:"0";s:11:"ROU_TO_PORT";s:1:"1";s:13:"ROU_FROM_PORT";s:1:"2";s:11:"ROU_EVN_UID";s:0:"";s:7:"GAT_UID";s:0:"";}}s:5:"lanes";a:0:{}s:8:"gateways";a:0:{}s:6:"inputs";a:0:{}s:7:"outputs";a:0:{}s:9:"dynaforms";a:3:{i:0;a:6:{s:7:"DYN_UID";s:32:"56344322151098d6e565331068089885";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:8:"DYN_TYPE";s:7:"xmlform";s:12:"DYN_FILENAME";s:65:"90503094551098570ac1a80013327982/56344322151098d6e565331068089885";s:9:"DYN_TITLE";s:22:"Test PMFRedirectToStep";s:15:"DYN_DESCRIPTION";s:0:"";}i:1;a:6:{s:7:"DYN_UID";s:32:"18713184351098ec39f2dd4047612642";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:8:"DYN_TYPE";s:7:"xmlform";s:12:"DYN_FILENAME";s:65:"90503094551098570ac1a80013327982/18713184351098ec39f2dd4047612642";s:9:"DYN_TITLE";s:15:"Middle Dynaform";s:15:"DYN_DESCRIPTION";s:0:"";}i:2;a:6:{s:7:"DYN_UID";s:32:"43849349851098f026bda85091768887";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:8:"DYN_TYPE";s:7:"xmlform";s:12:"DYN_FILENAME";s:65:"90503094551098570ac1a80013327982/43849349851098f026bda85091768887";s:9:"DYN_TITLE";s:10:"FINAL STEP";s:15:"DYN_DESCRIPTION";s:0:"";}}s:5:"steps";a:3:{i:0;a:8:{s:8:"STEP_UID";s:32:"531795309510992392a5874032553441";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:7:"TAS_UID";s:32:"3599909135109857407f524015663549";s:13:"STEP_TYPE_OBJ";s:8:"DYNAFORM";s:12:"STEP_UID_OBJ";s:32:"56344322151098d6e565331068089885";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:1;s:9:"STEP_MODE";s:4:"EDIT";}i:1;a:8:{s:8:"STEP_UID";s:32:"16310374051099241d828b1072782264";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:7:"TAS_UID";s:32:"3599909135109857407f524015663549";s:13:"STEP_TYPE_OBJ";s:8:"DYNAFORM";s:12:"STEP_UID_OBJ";s:32:"18713184351098ec39f2dd4047612642";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:2;s:9:"STEP_MODE";s:4:"EDIT";}i:2;a:8:{s:8:"STEP_UID";s:32:"8247895595109924611ec63097842789";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:7:"TAS_UID";s:32:"3599909135109857407f524015663549";s:13:"STEP_TYPE_OBJ";s:8:"DYNAFORM";s:12:"STEP_UID_OBJ";s:32:"43849349851098f026bda85091768887";s:14:"STEP_CONDITION";s:0:"";s:13:"STEP_POSITION";i:3;s:9:"STEP_MODE";s:4:"EDIT";}}s:8:"triggers";a:2:{i:0;a:7:{s:7:"TRI_UID";s:32:"32958399851098d4758b986031958094";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:8:"TRI_TYPE";s:6:"SCRIPT";s:10:"TRI_WEBBOT";s:368:"
+/***************************************************
+ *
+ * Generated by ProcessMaker Trigger Wizard
+ * Library: ProcessMaker Functions
+ * Method: PMF Redirect To Step
+ * Date: 2013-01-30 18:08:51
+ *
+ * ProcessMaker 2013
+ *
+ ****************************************************/
+
+PMFRedirectToStep (@@APPLICATION,@@INDEX,'DYNAFORM','43849349851098f026bda85091768887');";s:9:"TRI_PARAM";s:1406:"a:2:{s:4:"hash";s:32:"f3fffd14678981913a94b7dd9ff6bcff";s:6:"params";a:22:{s:25:"__notValidateThisFields__";s:0:"";s:22:"DynaformRequiredFields";s:2:"[]";s:9:"TRI_TITLE";s:19:"PMFRedirect to Step";s:7:"TRI_UID";s:32:"32958399851098d4758b986031958094";s:15:"TRI_DESCRIPTION";s:0:"";s:15:"sApplicationUID";s:13:"@@APPLICATION";s:15:"INSERT_VARIABLE";s:2:"@@";s:11:"iDelegation";s:7:"@@INDEX";s:9:"sStepType";s:8:"DYNAFORM";s:8:"sStepUid";s:32:"43849349851098f026bda85091768887";s:10:"TRI_ANSWER";s:0:"";s:15:"FIELDS_REQUIRED";s:46:"sApplicationUID,iDelegation,sStepType,sStepUid";s:14:"PMFUNTION_NAME";s:17:"PMFRedirectToStep";s:13:"LIBRARY_CLASS";s:21:"class.pmFunctions.php";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:11:"ALLFUNCTION";s:58:" $sApplicationUID , $iDelegation , $sStepType , $sStepUid ";s:14:"PAGED_TABLE_ID";s:15:"[object Object]";s:12:"LIBRARY_NAME";s:22:"ProcessMaker Functions";s:15:"PMFUNTION_LABEL";s:20:"PMF Redirect To Step";s:5:"savek";s:4:"Save";s:6:"cancel";s:6:"Cancel";s:10:"TRI_WEBBOT";s:368:"
+/***************************************************
+ *
+ * Generated by ProcessMaker Trigger Wizard
+ * Library: ProcessMaker Functions
+ * Method: PMF Redirect To Step
+ * Date: 2013-01-30 18:08:51
+ *
+ * ProcessMaker 2013
+ *
+ ****************************************************/
+
+PMFRedirectToStep (@@APPLICATION,@@INDEX,'DYNAFORM','43849349851098f026bda85091768887');";}}";s:9:"TRI_TITLE";s:19:"PMFRedirect to Step";s:15:"TRI_DESCRIPTION";s:0:"";}i:1;a:7:{s:7:"TRI_UID";s:32:"76696921151099bc5b6ccd4020639030";s:7:"PRO_UID";s:32:"90503094551098570ac1a80013327982";s:8:"TRI_TYPE";s:6:"SCRIPT";s:10:"TRI_WEBBOT";s:223:"$process=@@PROCESS;
+$task=@@TASK;
+
+$query = "SELECT 
+max(STEP_POSITION) as POSITION
+FROM STEP
+WHERE PRO_UID = '".@@PROCESS."' AND TAS_UID = '".@@TASK."' "; 
+
+$result = executeQuery($query);
+
+@@STEP = $result['1'][POSITION];";s:9:"TRI_PARAM";s:0:"";s:9:"TRI_TITLE";s:8:"Get Step";s:15:"TRI_DESCRIPTION";s:0:"";}}s:9:"taskusers";a:1:{i:0;a:4:{s:7:"TAS_UID";s:32:"3599909135109857407f524015663549";s:7:"USR_UID";s:32:"16601507650c85281879f33027092433";s:7:"TU_TYPE";s:1:"1";s:11:"TU_RELATION";s:1:"2";}}s:8:"groupwfs";a:1:{i:0;a:5:{s:7:"GRP_UID";s:32:"16601507650c85281879f33027092433";s:10:"GRP_STATUS";s:6:"ACTIVE";s:11:"GRP_LDAP_DN";s:0:"";s:6:"GRP_UX";s:6:"NORMAL";s:9:"GRP_TITLE";s:14:"Administrators";}}s:12:"steptriggers";a:2:{i:0;a:6:{s:8:"STEP_UID";s:32:"531795309510992392a5874032553441";s:7:"TAS_UID";s:32:"3599909135109857407f524015663549";s:7:"TRI_UID";s:32:"32958399851098d4758b986031958094";s:7:"ST_TYPE";s:5:"AFTER";s:12:"ST_CONDITION";s:0:"";s:11:"ST_POSITION";s:1:"1";}i:1;a:6:{s:8:"STEP_UID";s:32:"8247895595109924611ec63097842789";s:7:"TAS_UID";s:32:"3599909135109857407f524015663549";s:7:"TRI_UID";s:32:"76696921151099bc5b6ccd4020639030";s:7:"ST_TYPE";s:6:"BEFORE";s:12:"ST_CONDITION";s:0:"";s:11:"ST_POSITION";s:1:"1";}}s:13:"dbconnections";a:0:{}s:12:"reportTables";a:0:{}s:16:"reportTablesVars";a:0:{}s:14:"stepSupervisor";a:0:{}s:17:"objectPermissions";a:0:{}s:10:"subProcess";a:0:{}s:11:"caseTracker";a:0:{}s:17:"caseTrackerObject";a:0:{}s:5:"stage";a:0:{}s:14:"fieldCondition";a:0:{}s:5:"event";a:0:{}s:13:"caseScheduler";a:0:{}s:15:"processCategory";N;}00000003256344322151098d6e565331068089885000000600<?xml version="1.0" encoding="UTF-8"?>
+<dynaForm type="xmlform" name="90503094551098570ac1a80013327982/56344322151098d6e565331068089885" width="500" enabletemplate="0" mode="" nextstepsave="prompt">
+  <TEST_PMFREDIRECTTOSTEP type="title">
+    <en>Test PMFRedirect to Step</en>
+  </TEST_PMFREDIRECTTOSTEP>
+  <APPLICATION type="text" maxlength="64" validate="Any" size="35" mode="edit">
+    <en>Case Id</en>
+  </APPLICATION>
+  <INDEX type="text" maxlength="64" validate="Any" size="15" mode="edit">
+    <en>Delegation</en>
+  </INDEX>
+  <SUBMIT type="submit">
+    <en>SUBMIT</en>
+  </SUBMIT>
+</dynaForm>00000003218713184351098ec39f2dd4047612642000000629<?xml version="1.0" encoding="UTF-8"?>
+<dynaForm type="xmlform" name="90503094551098570ac1a80013327982/18713184351098ec39f2dd4047612642" width="500" enabletemplate="0" mode="" nextstepsave="prompt">
+  
+  <TEST_PMFREDIRECTTOSTEP type="title">
+    <en>Test PMFRedirect to Step Middle Dynaform</en>
+  </TEST_PMFREDIRECTTOSTEP>
+  <APPLICATION type="text" maxlength="64" validate="Any" size="35" mode="edit">
+    <en>Case Id</en>
+  </APPLICATION>
+  <INDEX type="text" maxlength="64" validate="Any" size="15" mode="edit">
+    <en>Delegation</en>
+  </INDEX>
+  <SUBMIT type="submit">
+    <en>SUBMIT MIDDLE</en>
+  </SUBMIT>
+  
+</dynaForm>00000003243849349851098f026bda85091768887000000716<?xml version="1.0" encoding="UTF-8"?>
+<dynaForm type="xmlform" name="90503094551098570ac1a80013327982/43849349851098f026bda85091768887" width="500" enabletemplate="0" mode="" nextstepsave="prompt">
+  <TEST_PMFREDIRECTTOSTEP type="title">
+    <en>Test PMFRedirect to Step Final Dynaform</en>
+  </TEST_PMFREDIRECTTOSTEP>
+  <APPLICATION type="text" maxlength="64" validate="Any" size="35" mode="edit">
+    <en>Case Id</en>
+  </APPLICATION>
+  <INDEX type="text" maxlength="64" validate="Any" size="15" mode="edit">
+    <en>Delegation</en>
+  </INDEX>
+  <STEP type="text" maxlength="64" validate="Any" size="15" mode="edit">
+    <en>STEP</en>
+  </STEP>
+  <SUBMIT type="submit">
+    <en>SUBMIT</en>
+  </SUBMIT>
+</dynaForm>MAILTEMPL000000018alert_message.html000000682
+<table style=\"background-color: white; font-family: Arial,Helvetica,sans-serif; color: black; font-size: 11px; text-align: left;\" cellpadding='10' cellspacing='0' width='100%'>
+<tbody>	
+		<tr><td style='font-size: 14px;'><b>ALERT MESSAGE</b></td></tr>
+		<tr><td style='vertical-align:middel;'>
+				<hr>
+				<br />
+						The Case <b>@#APP_NUMBER</b>, currently in the task <b>@#TAS_TITLE</b>, expired on <b>@#DEL_TASK_DUE_DATE</b>.						
+				<br />		
+				<br />
+				<hr><b>This Business Process is powered by ProcessMaker.<b><br />
+						<a href='http://www.processmaker.com' style='color:#c40000;'>www.processmaker.com</a>
+				<br />
+		</td></tr>
+</tbody>
+</table>000000022unassignedMessage.html000000829<table width="100%" cellpadding="10" cellspacing="0" style="background: #FFFFFF; font: 11px arial, verdana, helvetica, sans-serif;">
+    <tr>
+        <td style="font-size: 14px;">
+            <strong>UNASSIGNED CASE</strong>
+            <hr />
+        </td>
+    </tr>
+    <tr>
+        <td>
+            The case <strong>#@#APP_NUMBER</strong> is available on the unassigned list.
+            <br />
+            <br />
+            Note: If you want to claim the case and it is not available on the list, it might be already taken.
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <hr />
+            This Business Process is powered by ProcessMaker.
+            <br />
+            <a href="http://www.processmaker.com" style="color: #C40000;">www.processmaker.com</a>
+        </td>
+    </tr>
+</table>
